23e4a4080d60e5737d76c2d130aca65e62d9ff86
charitha omprakash <39599243+charithaomprakash@users.noreply.github.com>
2023-08-28 18:36:10 +0200

    tutorial added

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 09:23:54 2021
+
+@author: compraka
+"""
+from utils import *
+from ROI_marking_utils import *
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ET_helping_behavior",
+    version='1.0',
+    packages=find_packages(),
+    entry_points={"console_scripts": "align_coordinates = align_coordinates:main"},
+    author="C.Omprakash",
+    description="align_coordinates",
+    url="https://https://github.com/charithaomprakash/align_coordinates",
+    setup_requires=[
+        "pytest",
+    ],	
+    install_requires=[
+        "pytest-shutil",
+        "numpy",
+        "matplotlib",
+        "pathlib",
+        "pandas",
+        "ruamel.yaml"
+    ],
+)
