c44ff06170b3dbd6147b9dcc8ba6244350e909b7
charitha omprakash <39599243+charithaomprakash@users.noreply.github.com>
2023-08-31 09:56:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ET_helping_behavior",
+    name="align?coordinates",
     version='1.0',
     packages=find_packages(),
     entry_points={"console_scripts": "align_coordinates = align_coordinates:main"},
